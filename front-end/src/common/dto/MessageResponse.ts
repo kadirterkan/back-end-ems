0cafda26f53596ab8a807902776b4ae7fdac36f0
kadirterkan <kadirtayyiperkan@hotmail.com>
2021-09-07 11:00:22 +0300

    NOT THE PRESENTED VERSION THANKS TO GIT. MUST BE FIXED

--- front-end/src/common/dto/MessageResponse.ts
+++
@@ -1,9 +0,0 @@
-export interface MessageResponse {
-    messageType: MessageType;
-    message: string
-}
-
-export enum MessageType {
-    SUCCESS = "SUCCESS", INFO = "INFO" , WARNING = "WARNING" , ERROR= "ERROR",LOGIN_MOD="LOGIN_MOD",LOGIN_USER="LOGIN_USER",
-    ERROR_400 = "ERROR_400", ERROR_403 = "ERROR_403" , ERROR_500 = "ERROR_500"
-}
